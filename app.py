1b0b0dc57be2ce50ad37a1753905d18eb5f83e24
Daniel Matias <losiek86@gmail.com>
2019-01-17 10:31:48 +0000

    added styles and new templates

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DGNAME"] = 'task_manager'
+app.config["MONGO_URI"] = 'mongodb://admin:Losiek86@ds251284.mlab.com:51284/task_manager'
 
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+@app.route('/add_task')
+def add_task():
+    _categories = mongo.db.categories.find()
+    category_list = [category for category in _categories]
+    return render_template("addtask.html", categories = category_list)
+    
+@app.route('/insert_task', methods=["POST"])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
+    
+@app.route('/edit_task/<task_id>')
+def edit_task(task_id):
+    the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('edittask.html', task=the_task, categories=all_categories)
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
